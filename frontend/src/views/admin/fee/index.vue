88599f7b6c82b79c9b39de4452aeea96c74bdc6f
Alius Lee <ouo1@aliyun.com>
2024-05-16 23:49:53 +0800

    fix(style): fix some ui/ux

--- frontend/src/views/admin/fee/index.vue
+++ frontend/src/views/admin/fee/index.vue
@@ -1,19 +1,3 @@
 <template>
-  <div class="mb-3 mt-4">
-    <router-view />
-  </div>
+  <router-view />
 </template>
-
-<script setup lang="ts">
-// 面包屑
-const routes = [
-  {
-    path: '/admin/fee',
-    label: '会费管理'
-  }
-]
-</script>
-
-<style scoped lang="less">
-
-</style>
--- frontend/src/views/admin/user/index.vue
+++ frontend/src/views/admin/user/index.vue
@@ -1,26 +1,3 @@
 <template>
-  <a-space>
-    <custom-route-link to="/admin/user/manage">
-      用户管理
-    </custom-route-link>
-    <custom-route-link to="/admin/user/invitationCode">
-      邀请码管理
-    </custom-route-link>
-  </a-space>
-  <div class="mb-3 mt-4">
-    <router-view />
-  </div>
+  <router-view />
 </template>
-
-<script setup lang="ts">
-import type { BreadcrumbRoute } from '@arco-design/web-vue'
-import customRouteLink from '@/components/customRouteLink.vue'
-
-// 面包屑
-const routes: BreadcrumbRoute[] = [
-  {
-    path: '/admin/user',
-    label: '用户管理'
-  }
-]
-</script>
